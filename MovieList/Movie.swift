edbe4901c1649be6cbec36bf62901f0aabbab9fe
Dylan Kullas <DylanKullas13@gmail.com>
2026-01-15 21:40:11 -0500

    test that workflow will fail

--- MovieList/Movie.swift
+++ MovieList/Movie.swift
@@ -5,7 +5,7 @@
 //  Created by Dylan Kullas on 1/1/26.
 //
 
-struct Movie: Identifiable, Hashable {
+strct Movie: Identifiable, Hashable {
     // Properties stored data
     let mediaType: String
     let title: Substring
